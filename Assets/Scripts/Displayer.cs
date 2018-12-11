a26f9c4b9fa48f1e91a8a272a4aafe7b1ea33643
ThapeloBayford <thapelo@apptswana.com>
2018-12-11 23:36:26 +0200

    all label working !!

--- Assets/Scripts/Displayer.cs
+++ Assets/Scripts/Displayer.cs
@@ -9,7 +9,7 @@ public class Displayer : MonoBehaviour {
 
 
 // Update is called once per frame
-	void Start(){
+	void Start(){//initaite all game counters
 	//	Money = gameObject.GetComponent<TextMeshProUGUI> ();
 		Currency.text = PlayerPrefs.GetInt("Currency").ToString()+"K";
 		Networth.text = PlayerPrefs.GetInt("Networth").ToString()+"K";
@@ -28,14 +28,120 @@ public class Displayer : MonoBehaviour {
        
 	}
 
-	public void addHouse(){
+	public void addHouse(){//method to add houses to the players inventory will also delete from credits
 		int a = PlayerPrefs.GetInt("numberOfHouses");
-		int b = PlayerPrefs.GetInt("priceOfHouses")
-		if (b>a){
+		int b = PlayerPrefs.GetInt("priceOfHouses");
+		int c = PlayerPrefs.GetInt("Currency");
+		if (c>b){
 			a++;
+			c=c-b;
 			PlayerPrefs.SetInt("numberOfHouses",a);
 			numberOfHouses.text = PlayerPrefs.GetInt("numberOfHouses").ToString();
+			PlayerPrefs.SetInt("Currency",c);
+			Currency.text = PlayerPrefs.GetInt("Currency").ToString()+"K";
+
 		}
 
 	}
+		public void addCar(){//method to add car to the players inventory will also delete from credits
+		int a = PlayerPrefs.GetInt("numberOfCars");
+		int b = PlayerPrefs.GetInt("priceOfCars");
+		int c = PlayerPrefs.GetInt("Currency");
+		if (c>b){
+			a++;
+			c=c-b;
+			PlayerPrefs.SetInt("numberOfCars",a);
+			numberOfCars.text = PlayerPrefs.GetInt("numberOfCars").ToString();
+			PlayerPrefs.SetInt("Currency",c);
+			Currency.text = PlayerPrefs.GetInt("Currency").ToString()+"K";
+
+		}
+		}
+				public void addBusiness(){//method to add business to the players inventory will also delete from credits
+		int a = PlayerPrefs.GetInt("numberOfBusiness");
+		int b = PlayerPrefs.GetInt("priceOfBusiness");
+		int c = PlayerPrefs.GetInt("Currency");
+		if (c>b){
+			a++;
+			c=c-b;
+			PlayerPrefs.SetInt("numberOfBusiness",a);
+			numberOfBusiness.text = PlayerPrefs.GetInt("numberOfBusiness").ToString();
+			PlayerPrefs.SetInt("Currency",c);
+			Currency.text = PlayerPrefs.GetInt("Currency").ToString()+"K";
+
+		}
+				}
+		public void addStock(){//method to add stock to the players inventory will also delete from credits
+		int a = PlayerPrefs.GetInt("numberOfStocks");
+		int b = PlayerPrefs.GetInt("priceOfStocks");
+		int c = PlayerPrefs.GetInt("Currency");
+		if (c>b){
+			a++;
+			c=c-b;
+			PlayerPrefs.SetInt("numberOfStocks",a);
+			numberOfStocks.text = PlayerPrefs.GetInt("numberOfStocks").ToString();
+			PlayerPrefs.SetInt("Currency",c);
+			Currency.text = PlayerPrefs.GetInt("Currency").ToString()+"K";
+
+		}
+		}
+			public void removeHouse(){//method to remove House to the players inventory will also delete from credits
+		int a = PlayerPrefs.GetInt("numberOfHouses");
+		int b = PlayerPrefs.GetInt("priceOfHouses");
+		int c = PlayerPrefs.GetInt("Currency");
+		if (a>0){
+			a--;
+			c=c+b;
+			PlayerPrefs.SetInt("numberOfHouses",a);
+			numberOfHouses.text = PlayerPrefs.GetInt("numberOfHouses").ToString();
+			PlayerPrefs.SetInt("Currency",c);
+			Currency.text = PlayerPrefs.GetInt("Currency").ToString()+"K";
+
+		}
+
+	}
+		public void removeCar(){//method to remove car to the players inventory will also delete from credits
+		int a = PlayerPrefs.GetInt("numberOfCars");
+		int b = PlayerPrefs.GetInt("priceOfCars");
+		int c = PlayerPrefs.GetInt("Currency");
+		if (a>0){
+			a--;
+			c=c+b;
+			PlayerPrefs.SetInt("numberOfCars",a);
+			numberOfCars.text = PlayerPrefs.GetInt("numberOfCars").ToString();
+			PlayerPrefs.SetInt("Currency",c);
+			Currency.text = PlayerPrefs.GetInt("Currency").ToString()+"K";
+
+		}}
+				public void removeBusiness(){//method to remove Business to the players inventory will also delete from credits
+		int a = PlayerPrefs.GetInt("numberOfBusiness");
+		int b = PlayerPrefs.GetInt("priceOfBusiness");
+		int c = PlayerPrefs.GetInt("Currency");
+		if (a>0){
+			a--;
+			c=c+b;
+			PlayerPrefs.SetInt("numberOfBusiness",a);
+			numberOfBusiness.text = PlayerPrefs.GetInt("numberOfBusiness").ToString();
+			PlayerPrefs.SetInt("Currency",c);
+			Currency.text = PlayerPrefs.GetInt("Currency").ToString()+"K";
+
+		}
+				}
+		public void removeStock(){//method to remove Business to the players inventory will also delete from credits
+		int a = PlayerPrefs.GetInt("numberOfStocks");
+		int b = PlayerPrefs.GetInt("priceOfStocks");
+		int c = PlayerPrefs.GetInt("Currency");
+		if (a>0){
+			a--;
+			c=c+b;
+			PlayerPrefs.SetInt("numberOfStocks",a);
+			numberOfStocks.text = PlayerPrefs.GetInt("numberOfStocks").ToString();
+			PlayerPrefs.SetInt("Currency",c);
+			Currency.text = PlayerPrefs.GetInt("Currency").ToString()+"K";
+
+		}
+		}
+
+
+	
 }
